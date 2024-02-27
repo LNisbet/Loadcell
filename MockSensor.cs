6d4f22a4abf6f51c0d28166acdb3a9108baf305f
LNisbet <luke.nisbet@stfc.ac.uk>
2024-02-27 20:34:21 +0000

    First Commit

---
+++ MockSensor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadCell
+{
+    internal class MockSensor : ISensor
+    {
+        private Random random = new();
+        private float scaleValue = 1;
+
+        float ISensor.Value => (float)random.NextDouble() * scaleValue;
+
+        void ISensor.SetScale(float ScaleValue)
+        {
+            scaleValue = ScaleValue;
+        }
+
+        void ISensor.SetZero()
+        {
+            ;
+        }
+    }
+}
